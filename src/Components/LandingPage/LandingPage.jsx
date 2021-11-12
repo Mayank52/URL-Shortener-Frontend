a0e2d9790a7c80ff22fda1d0d90e57c80049fa07
Mayank52 <mayank.aggarwal963@gmail.com>
2021-11-12 18:00:01 +0530

    deploying

---
+++ src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import UrlShortener from '../UrlShortener/UrlShortener'
+import './LandingPage.css'
+
+export default function LandingPage() {
+    return (
+        <div>
+            <UrlShortener/>
+        </div>
+    )
+}
---
+++ src/Components/UrlShortener/UrlShortener.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./UrlShortener.css";
+
+export default function UrlShortener() {
+  const [shortUrl, setShortUrl] = useState("");
+  const [longUrl, setLongUrl] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [copyBtnText, setCopyBtnText] = useState("Copy");
+
+  const getShortUrl = async () => {
+    try {
+      if (!longUrl) {
+        setErrorMessage("No Url Entered");
+        return;
+      }
+
+      let res = await axios.post("/api/shorten", { longUrl });
+      console.log(res);
+
+      if (res.data === "Invalid url") {
+        setErrorMessage("Enter a valid url");
+        setShortUrl("");
+      } 
+      else {
+        setShortUrl(res.data.shortUrl);
+        setErrorMessage("");
+        setLongUrl("");
+        setCopyBtnText("Copy")
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="main">
+      <h1>Link Shortener</h1>
+      <div className="url-input">
+        <input
+          type="text"
+          id="long-url"
+          value={longUrl}
+          onChange={(e) => {
+            setLongUrl(e.target.value);
+            e.target.value = "";
+          }}
+        />
+      </div>
+      <div className="generate-url btn">
+        <button onClick={getShortUrl}>Shorten Link</button>
+      </div>
+      <div className="error">{errorMessage}</div>
+      {shortUrl ? (
+        <div className="short-url-div">
+          <h2>Short Link</h2>
+          <div className="short-url">{shortUrl}</div>
+          <div className="copy-btn btn">
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(shortUrl);
+                setCopyBtnText("Copied!");
+              }}
+            >
+              {copyBtnText}
+            </button>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
